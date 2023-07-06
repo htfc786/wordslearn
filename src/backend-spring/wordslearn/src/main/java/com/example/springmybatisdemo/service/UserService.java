6809974da420e657e81f59a19b57b88d7f92540a
htfc786 <2721672639@qq.com>
2023-07-06 22:42:44 +0800

    wordslearn 又改框架hhh

--- src/backend-spring/wordslearn/src/main/java/com/example/springmybatisdemo/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.springmybatisdemo.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.springmybatisdemo.entity.User;
-public interface UserService extends IService<User> {
-}
-
--- src/backend/project/__init__.py
+++
@@ -1,3 +0,0 @@
-import pymysql
-pymysql.version_info = (1, 4, 13, "final", 0) #解决报错 https://cloud.tencent.com/developer/article/1755110
-pymysql.install_as_MySQLdb() 
