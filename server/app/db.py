ebc4b1fdf87e1187fac393f300f4d486486b66fd
htfc786 <2721672639@qq.com>
2023-07-13 15:53:27 +0800

    wordslearn 单词、组、书、音频的数据库设计完成

--- server/app/db.py
+++ server/app/db.py
@@ -10,9 +10,45 @@ pymysql.install_as_MySQLdb()
 db = SQLAlchemy()
 
 class Users(db.Model):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    username = db.Column(db.String(16))
-    password = db.Column(db.String(64))
-    salt = db.Column(db.String(6))
-    isadmin = db.Column(db.Boolean, default=False)
+    username = db.Column(db.String(16), nullable=False)
+    password = db.Column(db.String(64), nullable=False)
+    salt = db.Column(db.String(6), nullable=False)
+    isadmin = db.Column(db.Boolean, default=False, nullable=False)
+
+class Words(db.Model):
+    __tablename__ = 'words'
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    word = db.Column(db.String(64), nullable=False)
+    pronounce = db.Column(db.String(64), nullable=True)
+    chinese = db.Column(db.String(128), nullable=True)
+    note = db.Column(db.String(255), nullable=True)
+    type = db.Column(db.Boolean, default=True, nullable=False) # 单词类型 1-单词 0-词组
+    sound_id = db.Column(db.Integer, nullable=True)
+    sound_start = db.Column(db.Float, nullable=True)
+    sound_end = db.Column(db.Float, nullable=True)
+    groupid = db.Column(db.Integer, nullable=False)
+    bookid = db.Column(db.Integer, nullable=False)
+
+class Groups(db.Model):
+    __tablename__ = 'groups'
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String(32), nullable=False)
+    description = db.Column(db.String(255), nullable=True)
+    bookid = db.Column(db.Integer, nullable=False)
+
+class Groups(db.Model):
+    __tablename__ = 'books'
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String(32), nullable=False)
+    description = db.Column(db.String(255), nullable=True)
+    cover = db.Column(db.String(2048), nullable=True)
+
+
+class Sounds(db.Model):
+    __tablename__ = 'books'
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    url = db.Column(db.String(2048), nullable=False)
+
+
--- sql/init.sql
+++ sql/init.sql
@@ -3,7 +3,7 @@ CREATE DATABASE wordslearn;
 
 USE wordslearn;
 
-CREATE TABLE `user` (
+CREATE TABLE `users` (
   `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
   `username` char(16) NOT NULL COMMENT '用户名',
   `password` char(64) NOT NULL COMMENT '密码-可以使用SHA256',
@@ -11,3 +11,41 @@ CREATE TABLE `user` (
   `isadmin` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否管理员',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB CHARSET=utf8mb4;
+
+CREATE TABLE `words` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `word` char(64) NOT NULL COMMENT '单词',
+  `pronounce` char(64) COMMENT '音标',
+  `chinese` char(128) COMMENT '中文',
+  `note` char(255) COMMENT '备注',
+  `type` tinyint(1) NOT NULL COMMENT '单词类型 1-单词 0-词组',
+  `sound_id` int COMMENT '音频id',
+  `sound_start` float COMMENT '音频开始时间',
+  `sound_end` float COMMENT '音频结束时间',
+  `groupid` int NOT NULL COMMENT '所属组id',
+  `bookid` int NOT NULL COMMENT '所属书id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB CHARSET=utf8mb4;
+
+CREATE TABLE `groups` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `name` char(32) NOT NULL COMMENT '组名称',
+  `description` char(255) COMMENT '组描述',
+  `bookid` int COMMENT '所属书id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB CHARSET=utf8mb4;
+
+CREATE TABLE `books` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `name` char(32) NOT NULL COMMENT '书名称',
+  `description` char(255) COMMENT '书描述',
+  `cover` varchar(2048) COMMENT '书封面 url',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB CHARSET=utf8mb4;
+
+CREATE TABLE `sounds` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `url` varchar(2048) NOT NULL COMMENT '音频url',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB CHARSET=utf8mb4;
+
